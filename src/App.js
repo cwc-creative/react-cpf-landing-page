64fd6da72958ab64b8632ce3a246fbc697653791
Heriniaina Jean Bosco FANOMEZANTSOA <herifanomezantsoaf.com>
2022-02-21 08:46:08 +0100

    REFACTOR: small changes to enhance user experience

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          CPF | Votre formation en ligne
-        </a>
-      </header>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
